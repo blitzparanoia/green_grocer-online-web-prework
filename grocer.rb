69255eceefb393ee1f85e2f7a9ff711137d150e1
Irene Saez <bluemilk44@gmail.com>
2019-03-08 22:50:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,40 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+  cart.each_with_object({}) do |item, result|
+    item.each do |food, description|
+      if result[food]
+        description[:count] += 1
+      else
+        description[:count] = 1
+        result[food] = description
+      end
+      end
+      end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |item, description|
+    binding.pry
 end
 
-def checkout(cart, coupons)
+#def checkout(cart, coupons)
   # code here
-end
+#end
